5134e7bb2867c4917809479267518ba7a3f3d54f
alx <alx.yulax@gmail.com>
2025-12-23 21:09:33 +0700

    Stage 7: error3

--- run_web.py
+++ run_web.py
@@ -2,10 +2,10 @@ from app.web.app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000,
-        debug=True,
-    )
+# if __name__ == "__main__":
+#     app.run(
+#         host="0.0.0.0",
+#         port=5000,
+#         debug=True,
+#     )
 
